0084ac80d8511b15b339ae27c5ed1e815a84d76c
juampymdd <juampymdd@gmail.com>
2021-12-28 21:42:43 -0300

    Component compound ready

---
+++ src/02-component-patterns/components/index.ts
@@ -0,0 +1,18 @@
+import { ProductButtons } from './ProductButtons';
+import { ProductImage } from './ProductImage';
+import { ProductTitle } from './ProductTitle';
+import  { ProductCard as ProductCardHOC } from './ProductCard';
+import { ProductCardHOCProps } from '../interfaces/iterfaces';
+
+export { ProductButtons } from './ProductButtons';
+export { ProductImage } from './ProductImage';
+export { ProductTitle } from './ProductTitle';
+
+export const ProductCard: ProductCardHOCProps = Object.assign(ProductCardHOC, {
+    Title: ProductTitle,
+    Image: ProductImage,
+    Buttons: ProductButtons
+})
+
+export default ProductCard;
+
---
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,36 @@
+
+import { createContext } from 'react';
+import { ProductCard, ProductImage, ProductButtons, ProductTitle } from '../components/index';
+
+const product = {
+    id: '1',
+    title: 'Coffee Mug - Card',
+    img: '../coffee-mug.png'
+}
+
+export const ShoppingPage = () => {
+    return (
+        <div>
+            <h1>Shopping Page</h1>
+            <hr />
+            <div style={{
+                display: 'flex',
+                flexDirection: 'row',
+                flexWrap: 'wrap'
+            }}>
+                <ProductCard product={product}>
+                    <ProductCard.Image  />
+                    <ProductCard.Title title={'Hola Mundo'}/>
+                    <ProductCard.Buttons />
+                </ProductCard>
+
+                <ProductCard product={product}>
+                    <ProductImage  />
+                    <ProductTitle title={''}/>
+                    <ProductButtons />
+                </ProductCard>
+            </div>
+            
+        </div>
+    )
+}
