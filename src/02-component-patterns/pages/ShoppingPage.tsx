ee453945dd648be47d88cfcaec6292f714caf4a4
juampymdd <juampymdd@gmail.com>
2022-01-20 11:29:47 -0300

    pre-nmp listo

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -17,7 +17,6 @@ export const ShoppingPage = () => {
                 <ProductCard 
                     key={ product.id }
                     product={ product }
-                    className='bg-dark text-white'
                     initialValues={{
                         count: 4,
                         maxCount: 10
@@ -26,17 +25,9 @@ export const ShoppingPage = () => {
                     {
                         ( {reset, increaseBy, count, isMaxCountReached, maxCount} ) => (
                             <>
-                                <ProductImage  className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0, 0.2)' }}/>
-                                <ProductTitle 
-                                    className="text-white text-bold"
-                                />
-                                <ProductButtons className="custom-buttons" />
-                                <button onClick={ reset }> Reset</button>
-                                <button onClick={ ()=> increaseBy(-2) }> -2</button>
-                                {
-                                    (!isMaxCountReached && <button onClick={ ()=> increaseBy(+2) }> +2</button>)
-                                }
-                                <span>{ count } - { maxCount }</span>
+                                <ProductImage />
+                                <ProductTitle />
+                                <ProductButtons />
                             </>
                         )
                     }
